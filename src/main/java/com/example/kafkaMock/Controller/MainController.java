c96e5e0ce5715d699d32031b92b1b37c4469c22c
kseniagrabalina <ksenyapuk@mail.ru>
2025-09-22 17:16:34 +0300

    3rd

--- src/main/java/com/example/kafkaMock/Controller/MainController.java
+++ src/main/java/com/example/kafkaMock/Controller/MainController.java
@@ -1,16 +1,16 @@
 package com.example.kafkaMock.Controller;
 
+import com.example.kafkaMock.Kafka.KafkaSender;
 import com.example.kafkaMock.Model.RequestDTO;
 import com.example.kafkaMock.Model.ResponseDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,16 +24,9 @@ public class MainController {
     private Logger log = LoggerFactory.getLogger(MainController.class);
 
     ObjectMapper mapper = new ObjectMapper();
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final String topic;
 
-    public MainController(KafkaTemplate<String, String> kafkaTemplate,
-                          ObjectMapper mapper,
-                          @Value("${kafka.topic:test}") String topic) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.mapper = mapper;
-        this.topic = topic;
-    }
+    @Autowired
+    private KafkaSender kafkaSender;
 
     @PostMapping(
             value = "/json/info/postBalances",
@@ -43,31 +36,24 @@ public class MainController {
     public ResponseEntity<?> postBalances(@RequestBody RequestDTO requestDTO,
                                                     HttpServletRequest request) throws JsonProcessingException {
         try {
-            ResponseDTO responseDTO = new ResponseDTO();
 
             String msg_id = requestDTO.getMsg_id();
-            responseDTO.setMsg_id(msg_id);
-
             long timestamp = System.currentTimeMillis();
-            responseDTO.setTimestamp(timestamp);
-
             String method = request.getMethod();
-            responseDTO.setMethod(method);
-
             String uri = request.getRequestURI();
-            responseDTO.setUri(uri);
+
+            ResponseDTO responseDTO = new ResponseDTO(msg_id, timestamp, method, uri);
 
             String kafkaMessage = mapper.writeValueAsString(responseDTO);
-            kafkaTemplate.send(topic, kafkaMessage);
-
+            kafkaSender.sendMessage("test", kafkaMessage);
 
             log.info("Message sent to Kafka: {}", kafkaMessage);
             log.error("*********** RequestDTO **********" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
             log.error("*********** ResponseDTO **********" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
 
-            return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+            return ResponseEntity.status(HttpStatus.OK).build();
     } catch (Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        return ResponseEntity.status(500).build();
         }
     }
 }
--- src/main/java/com/example/kafkaMock/Model/ResponseDTO.java
+++ src/main/java/com/example/kafkaMock/Model/ResponseDTO.java
@@ -1,8 +1,10 @@
 package com.example.kafkaMock.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ResponseDTO {
 
     private String msg_id;
